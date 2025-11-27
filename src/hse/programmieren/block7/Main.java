0f8d10b3ddd4a476701b72d190c993b0f42d6763
Klemens Morbe <klemens.morbe@pep-digital.de>
2025-11-27 23:17:58 +0100

    - add homework;

---
+++ src/hse/programmieren/block7/Main.java
@@ -0,0 +1,11 @@
+package hse.programmieren.block7;
+
+import hse.programmieren.block7.exercise.Hausi;
+
+public class Main {
+
+    static void main() {
+        System.out.println(Hausi.process(1123));
+        System.out.println(Hausi.process(123,0));
+    }
+}
---
+++ src/hse/programmieren/block7/exercise/Hausi.java
@@ -0,0 +1,24 @@
+package hse.programmieren.block7.exercise;
+
+public class Hausi {
+
+    public static int process(int number) {
+        int number2 = 0;
+
+        while (number > 0) {
+            number2 = number2 * 10 + (number % 10);
+            number = number / 10;
+        }
+
+        return number2;
+    }
+
+    public static int process(int number1, int number2) {
+        if (number1 == 0) {
+            return number2;
+        }
+        return process(number1 / 10, number2 * 10 + number1 % 10);
+    }
+
+
+}
